ce5a173f26bd68608801355f8c7376599d722eac
thameemk612 <thameemk612@gmail.com>
2019-08-02 19:15:12 +0530

    htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ece-2019/';
+$config['base_url'] = 'http://localhost/Student-alumni-portal-ECETKMCE-v2/';
 
 /*
 |--------------------------------------------------------------------------
