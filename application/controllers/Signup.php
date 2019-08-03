5b336102c228e7f399318f97560f1e92be8b8a74
thameemk612 <thameemk612@gmail.com>
2019-08-03 23:02:38 +0530

    duplicate entry fix

--- application/controllers/Signup.php
+++ application/controllers/Signup.php
@@ -15,31 +15,45 @@ class Signup extends CI_Controller {
         $this->load->view('signup');
     }
     public function process(){
+      $data = $this->input->post();
+      $data = $this->security->xss_clean($data);
+      $this->form_validation->set_rules('user_email','User Email','required|is_unique[login_users.user_email]');
+      if($this->form_validation->run() == FALSE){
+        $this->session->set_flashdata('msgreq', 'You have already registred');
+        redirect('signup');
+      }
+      else{
       $this->form_validation->set_rules('password', 'Password', 'required');
       $this->form_validation->set_rules('passconf', 'Re-enter', 'required|matches[password]');
-      $this->form_validation->set_rules('firstname','First Name','required');
-      $this->form_validation->set_rules('lastname','Last Name','required');
-      $this->form_validation->set_rules('user_email','User Email','required');
-      $this->form_validation->set_rules('phone','Phone Number','required');
-      $this->form_validation->set_rules('passyear','Pass year','required');
-      if($this->form_validation->run() == FALSE){
-           $this->session->set_flashdata('msgreq', 'Fill all required fields!');
-           redirect('signup');
-       }
-      else {
-          $data = array(
-            'first_name' => $this->input->post('firstname'),
-            'last_name' => $this->input->post('lastname'),
-            'user_company' => $this->input->post('company'),
-            'user_email' => $this->input->post('user_email'),
-            'phone' => $this->input->post('phone'),
-            'year_pass' => $this->input->post('passyear'),
-          );
-          $data=$this->security->xss_clean($data);
-          $this->report_model->userRegister($data);
-          $this->session->set_flashdata('msg', 'Registration Success!');
-          redirect('signup');
-        }
+          if($this->form_validation->run() == FALSE){
+            $this->session->set_flashdata('msgreq', 'Password doesnot match');
+            redirect('signup');
+          }
+          else {
+            $this->form_validation->set_rules('firstname','First Name','required');
+            $this->form_validation->set_rules('lastname','Last Name','required');
+            $this->form_validation->set_rules('phone','Phone Number','required');
+            $this->form_validation->set_rules('passyear','Pass year','required');
+                if($this->form_validation->run() == FALSE){
+                     $this->session->set_flashdata('msgreq', 'Fill all fields! or Check the password ');
+                     redirect('signup');
+                 }
+                else {
+                    $data = array(
+                      'first_name' => $this->input->post('firstname'),
+                      'last_name' => $this->input->post('lastname'),
+                      'user_company' => $this->input->post('company'),
+                      'user_email' => $this->input->post('user_email'),
+                      'phone' => $this->input->post('phone'),
+                      'year_pass' => $this->input->post('passyear'),
+                      'password' => $this->hash_password($password),
+                    );
+                    $this->report_model->userRegister($data);
+                    $this->session->set_flashdata('msg', 'Registration Success!');
+                    redirect('signup');
+                  }
+                }
     }
+  }
 
 }
