eda5cf58b81a1bd32c411ce77354f5d6448524aa
thameemk612 <thameemk612@gmail.com>
2019-08-04 00:34:39 +0530

    profilr ui

---
+++ application/views/user_home/profile.php
@@ -0,0 +1,119 @@
+
+<body>
+    <nav class="navigation">
+        <div class="navigation__column">
+            <a href="feed.html">
+                <img src="images/logo.png" />
+            </a>
+        </div>
+        <div class="navigation__column">
+            <i class="fa fa-search"></i>
+            <input type="text" placeholder="Search">
+        </div>
+        <div class="navigation__column">
+            <ul class="navigations__links">
+                <li class="navigation__list-item">
+                    <a href="explore.html" class="navigation__link">
+                        <i class="fa fa-compass fa-lg"></i>
+                    </a>
+                </li>
+                <li class="navigation__list-item">
+                    <a href="#" class="navigation__link">
+                        <i class="fa fa-heart-o fa-lg"></i>
+                    </a>
+                </li>
+                <li class="navigation__list-item">
+                    <a href="profile.html" class="navigation__link">
+                        <i class="fa fa-user-o fa-lg"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main id="profile">
+        <header class="profile__header">
+            <div class="profile__column">
+                <img src="images/avatar.jpg" />
+            </div>
+            <div class="profile__column">
+                <div class="profile__title">
+                    <h3 class="profile__username">serranoarevalo</h3>
+                    <a href="edit-profile.html">Edit profile</a>
+                    <i class="fa fa-cog fa-lg"></i>
+                </div>
+                <ul class="profile__stats">
+                    <li class="profile__stat">
+                        <span class="stat__number">333</span> posts
+                    </li>
+                    <li class="profile__stat">
+                        <span class="stat__number">1234</span> followers
+                    </li>
+                    <li class="profile__stat">
+                        <span class="stat__number">36</span> following
+                    </li>
+                </ul>
+                <p class="profile__bio">
+                    <span class="profile__full-name">
+                        Nicolás Serrano Arévalo
+                    </span> Doing whatever and eating Pho Lorem ipsum dolor sit amet consectetur, adipisicing
+                    elit. Ducimus suscipit praesentium eveniet quibusdam ipsam omnis fugit. Tempore voluptates ratione recusandae
+                    natus illo perspiciatis suscipit, odio consequuntur quasi obcaecati minus! Omnis.
+                    <a href="#">serranoarevalo.com</a>
+                </p>
+            </div>
+        </header>
+        <section class="profile__photos">
+            <div class="profile__photo">
+                <img src="images/feedPhoto.jpg" />
+                <div class="profile__photo-overlay">
+                    <span class="overlay__item">
+                        <i class="fa fa-heart"></i>
+                        486
+                    </span>
+                    <span class="overlay__item">
+                        <i class="fa fa-comment"></i>
+                        344
+                    </span>
+                </div>
+            </div>
+            <div class="profile__photo">
+                <img src="images/feedPhoto.jpg" />
+                <div class="profile__photo-overlay">
+                    <span class="overlay__item">
+                            <i class="fa fa-heart"></i>
+                            486
+                        </span>
+                    <span class="overlay__item">
+                            <i class="fa fa-comment"></i>
+                            344
+                        </span>
+                </div>
+            </div>
+            <div class="profile__photo">
+                <img src="images/feedPhoto.jpg" />
+                <div class="profile__photo-overlay">
+                    <span class="overlay__item">
+                                <i class="fa fa-heart"></i>
+                                486
+                            </span>
+                    <span class="overlay__item">
+                                <i class="fa fa-comment"></i>
+                                344
+                            </span>
+                </div>
+            </div>
+            <div class="profile__photo">
+                <img src="images/feedPhoto.jpg" />
+                <div class="profile__photo-overlay">
+                    <span class="overlay__item">
+                                    <i class="fa fa-heart"></i>
+                                    486
+                                </span>
+                    <span class="overlay__item">
+                                    <i class="fa fa-comment"></i>
+                                    344
+                                </span>
+                </div>
+            </div>
+        </section>
+    </main>
