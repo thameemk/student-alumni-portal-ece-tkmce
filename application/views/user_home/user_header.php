953fbdd0506afa244dbc81616bcf2a29b8fab4ee
thameemk612 <thameemk612@gmail.com>
2019-08-05 23:56:36 +0530

    link changed

--- application/views/user_home/user_header.php
+++ application/views/user_home/user_header.php
@@ -16,7 +16,7 @@
 <body>
     <nav class="navigation">
         <div class="navigation__column">
-            <a href="<?php echo base_url();?>user/home">
+            <a href="<?php echo base_url();?>muportal">
             <img src="<?php echo base_url ()?>assets/front/img/ece_logo.png" alt="" title="" />
             </a>
         </div>
