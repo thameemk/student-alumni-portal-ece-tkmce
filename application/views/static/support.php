1a32721f9199735ae08c21e2148f10f898d19892
thameemk612 <thameemk612@gmail.com>
2019-08-20 23:33:04 +0530

    support

---
+++ application/views/static/support.php
@@ -0,0 +1,16 @@
+<section class="banner-area relative about-banner mb-5" id="blog">
+	<div class="overlay overlay-bg"></div>
+	<div class="container">
+		<div class="row d-flex align-items-center justify-content-center">
+			<div class="about-content col-lg-12">
+				<h1 class="text-white">
+					Support
+				</h1>
+				<p class="text-white link-nav"><a href="<?php echo base_url();?>home">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="<?php echo base_url();?>support"> Support</a></p>
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<h4 class="mt-3 mb-3 text-center">For any web related queries contact <a href="mailto:webadmin@ecetkmce.in">webadmin@ecetkmce.in</a></h4>
+</section>
