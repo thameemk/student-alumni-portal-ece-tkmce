ee2d2856b55d6f9aaa69fe8e5d93fad774862c57
thameemk612 <thameemk612@gmail.com>
2020-04-18 00:41:29 +0530

    team pic added

--- application/views/static_new/about.php
+++ application/views/static_new/about.php
@@ -42,3 +42,90 @@
       </div>
     </div>
   </section>
+  
+  <div class="main-content">
+         <section>
+         <h2 class="iq-fw-8 mb-3" align="center">Meet the Team</h2>
+            <div class="container">
+               <div class="row">
+
+                  <div class="col-lg-3 col-md-6">
+                    <a target="blank" href="https://www.linkedin.com/in/siril-roy-b20712106/">
+                        <div class="team-box mb-4">
+                            <div class="team-img text-center">
+                              <img src="https://media-exp1.licdn.com/dms/image/C5103AQG3DyX52SpY3w/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=ItSSi2XzJ_l2c_B-Yvz2QKtBiqsiEEL1Op6EKVPAYKo" class="img-fluid rounded-circle" alt="image">
+                            </div>
+                            <div class="team-detail">                           
+                              <div class="team-info">
+                                  <h6 align="center"class="mb-0"><a href="https://www.linkedin.com/in/siril-roy-b20712106/">Siril Roy</a></h6>                              
+                              </div>
+                            </div>                       
+                        </div>
+                      </a>
+                  </div>
+
+
+                  <div class="col-lg-3 col-md-6">
+                    <a target="blank" href="https://www.linkedin.com/in/akhil-a-72b95211a/">
+                        <div class="team-box mb-4">
+                            <div class="team-img text-center">
+                              <img src="https://media-exp1.licdn.com/dms/image/C5103AQFDRqPNy12TWQ/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=8NJPV-zt3TARy2L0EX79kBth2lVN_eJ0KdPRPd7TS0g" class="img-fluid rounded-circle" alt="image">
+                            </div>
+                            <div class="team-detail">                           
+                              <div class="team-info">
+                                  <h6 align="center"class="mb-0"><a href="https://www.linkedin.com/in/akhil-a-72b95211a/">Akhil A</a></h6>                              
+                              </div>
+                            </div>                       
+                        </div>
+                      </a>
+                  </div>
+                  
+                  <div class="col-lg-3 col-md-6">
+                    <a target="blank" href="//thameem.me">
+                        <div class="team-box mb-4">
+                            <div class="team-img text-center">
+                              <img src="https://media-exp1.licdn.com/dms/image/C5103AQFiwp_KqCc5XA/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=DchkurSaefqECmTFWU1YiNr1lzS2BmPiWupnkRCTvN0" class="img-fluid rounded-circle" alt="image">
+                            </div>
+                            <div class="team-detail">                           
+                              <div class="team-info">
+                                  <h6 align="center"class="mb-0"><a href="//thameem.me">Thameem K</a></h6>                              
+                              </div>
+                            </div>                       
+                        </div>
+                      </a>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6">
+                    <a target="blank" href="https://www.linkedin.com/in/aravindvenma/">
+                        <div class="team-box mb-4">
+                            <div class="team-img text-center">
+                              <img src="https://media-exp1.licdn.com/dms/image/C5103AQGjl6Rlu_jg7w/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=dycU3Rv5cGLAU6zszka2Im7r4iv6sVKdXX218gckbxw" class="img-fluid rounded-circle" alt="image">
+                            </div>
+                            <div class="team-detail">                           
+                              <div class="team-info">
+                                  <h6 align="center"class="mb-0"><a href="https://www.linkedin.com/in/aravindvenma/">Aravind M</a></h6>                              
+                              </div>
+                            </div>                       
+                        </div>
+                      </a>
+                  </div>
+                                  
+                 
+                  <div class="col-lg-3 col-md-6">
+                    <a target="blank" href="https://www.linkedin.com/in/supreethbare/">
+                        <div class="team-box mb-4">
+                            <div class="team-img text-center">
+                              <img src="https://media-exp1.licdn.com/dms/image/C5103AQHbGpDog5V-ZA/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=3SLZtCPKOJPkPyS-1oxI2CDzl2ggKrLP-QddLtyTRgc" class="img-fluid rounded-circle" alt="image">
+                            </div>
+                            <div class="team-detail">                           
+                              <div class="team-info">
+                                  <h6 align="center"class="mb-0"><a href="https://www.linkedin.com/in/supreethbare/">Supreeth Bare</a></h6>                              
+                              </div>
+                            </div>                       
+                        </div>
+                      </a>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </div>
